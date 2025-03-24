e43fc9837ef479ae4a7f299ef1ab5efe4057cef6
Aviel Segev <asegev@redhat.com>
2025-03-24 18:36:22 +0200

    Add test case for disconnected environment
    Signed-off-by: Aviel Segev <asegev@redhat.com>

--- deploy/e2e.mk
+++ deploy/e2e.mk
@@ -1,7 +1,7 @@
 E2E_PRIVATE_KEY_FOLDER_PATH ?= /etc/planner/e2e
 
 .PHONY: deploy-e2e-environment
-deploy-e2e-environment: install_qemu_img ignore_insecure_registry create_kind_e2e_cluster setup_libvirt generate_private_key deploy_registry deploy_vcsim build_assisted_migration_containers deploy_assisted_migration persistent_disk
+deploy-e2e-environment: install_qemu_img ignore_insecure_registry create_kind_e2e_cluster setup_libvirt install_coreos_installer generate_private_key deploy_registry deploy_vcsim build_assisted_migration_containers deploy_assisted_migration persistent_disk
 
 .PHONY: install_qemu_img
 install_qemu_img:
@@ -32,6 +32,10 @@ setup_libvirt:
 		sudo dnf install -y sshpass libvirt-devel libvirt-daemon libvirt-daemon-config-network; \
 	fi
 	sudo systemctl restart libvirtd
+
+.PHONY: install_coreos_installer
+install_coreos_installer:
+	sudo $(PKG_MANAGER) install -y coreos-installer;
 
 .PHONY: generate_private_key
 generate_private_key:
