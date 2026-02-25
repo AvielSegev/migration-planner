d3b76a9d2c2a0a67139733e166490f32dc85cf82
Aviel Segev <asegev@redhat.com>
2026-02-25 15:03:42 +0200

    NO-JIRA | refactor: remove unused code
    Signed-off-by: Aviel Segev <asegev@redhat.com>

--- pkg/opa/recommendations.go
+++ pkg/opa/recommendations.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/kubev2v/forklift/pkg/controller/provider/model/vsphere"
 	"github.com/kubev2v/migration-planner/pkg/duckdb_parser/models"
 )
 
@@ -19,40 +18,6 @@ var OSUpgradeMap = map[string]string{
 	"centos 8":                   "Red Hat Enterprise Linux 8",
 	"centos 9":                   "Red Hat Enterprise Linux 9",
 	"amazon linux 2":             "Red Hat Enterprise Linux 8",
-}
-
-// getOSUpgradeConcern returns a Concern for OS upgrade recommendation if the OS is in the upgrade map
-func getOSUpgradeConcern(osName string) *vsphere.Concern {
-	osNameLower := strings.ToLower(strings.TrimSpace(osName))
-	upgradeTarget, found := OSUpgradeMap[osNameLower]
-	if !found {
-		for k, v := range OSUpgradeMap {
-			if strings.HasPrefix(osNameLower, k) {
-				upgradeTarget = v
-				break
-			}
-		}
-	}
-
-	if upgradeTarget == "" {
-		return nil
-	}
-
-	return &vsphere.Concern{
-		Id:         "vmware.os.upgrade.recommendation",
-		Category:   "Information",
-		Label:      "OS Upgrade Recommendation",
-		Assessment: fmt.Sprintf("The guest operating system: %s is not currently supported. The operating system can be upgraded to %s", osName, upgradeTarget),
-	}
-}
-
-// AddOSUpgradeConcernToVM adds OS upgrade recommendation concern to a VM if its OS is in the OSUpgradeMap
-func AddOSUpgradeConcernToVM(vm *vsphere.VM, osName string) {
-	// Get the upgrade concern if the OS is in the map
-	concern := getOSUpgradeConcern(osName)
-	if concern != nil {
-		vm.Concerns = append(vm.Concerns, *concern)
-	}
 }
 
 // GetOSUpgradeConcern returns an OS upgrade recommendation concern for models.VM if applicable.
